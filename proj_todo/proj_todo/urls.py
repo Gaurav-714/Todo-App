56ded2dc543e42807dafc21d10a64214aa628d9f
Gaurav-714 <gauravnagose714@gmail.com>
2024-01-25 01:46:09 +0530

    final commit

--- proj_todo/proj_todo/urls.py
+++ proj_todo/proj_todo/urls.py
@@ -1,22 +1,14 @@
-"""
-URL configuration for proj_todo project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from todo_app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', signup_page, name = 'signup'),
+    path('signin/', signin, name = 'signin'),
+    path('signin/', signout, name = 'signout'),
+    path('todo/', todo, name = 'todo'),
+    path('update/<int:srno>', update, name = 'update'),
+    path('delete/<int:srno>', delete, name = 'delete'),
 ]
--- proj_todo/todo_app/views.py
+++ proj_todo/todo_app/views.py
@@ -1,3 +1,98 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+from .models import *
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def signup_page(request):
+    if request.method == 'POST':
+
+        try:
+            username = request.POST.get('username')
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+            
+            user = User.objects.filter(username = username)
+            if user.exists():
+                messages.warning(request, "Username Is Already Taken.")
+                return redirect('/')
+            
+            user = User.objects.create(username = username, email = email)
+            user.set_password(password)
+            user.save()
+
+            messages.success(request, "Account Created.")
+            return redirect('/signin/')
+
+        except:
+            messages.warning(request, "Something Went Wrong...")
+            return redirect('/')
+        
+    return render(request, 'signup.html')
+
+
+@login_required(login_url = '/signin/')
+def signin(request):
+    if request.method == 'POST':
+
+        try:
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = User.objects.filter(username = username)
+            if not user.exists():
+                messages.warning(request, "Username Not Found.")
+                return redirect('/signin/')
+            
+            user = authenticate(username = username, password = password)
+            if user:
+                login(request, user)
+                return redirect('/todo/')
+            else:
+                messages.warning(request, "Incorrect Password")
+                return redirect('/signin/')
+        
+        except:
+            messages.warning(request, "Something Went Wrong...")
+            return redirect('/signin/')
+
+    return render(request, 'signin.html')
+
+
+def signout(request):
+    logout(request)
+    return redirect('/signin/')
+
+
+@login_required(login_url = '/signin/')
+def todo(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        obj = Todo(title = title, user = request.user)
+        obj.save()
+        todos = Todo.objects.filter(user = request.user).order_by('date')
+        return redirect('/todo/', {'todos' : todos})
+    
+    todos = Todo.objects.filter(user = request.user).order_by('date')
+    return render(request, 'todo.html', {'todos' : todos})
+
+
+@login_required(login_url = '/signin/')
+def update(request, srno):
+    if request.method == 'POST':
+
+        title = request.POST.get('title')
+        todos = Todo.objects.filter(user = request.user, srno = srno)
+        todos.title = title
+        todos.save()
+        return redirect('/todo/')
+    
+    todos = Todo.objects.filter(user = request.user).order_by('date')
+    return render(request, 'update.html', {'todos' : todos})
+
+
+@login_required(login_url = '/signin/')
+def delete(request, srno):
+    todos = Todo.objects.get(user = request.user, srno = srno)
+    todos.delete()
+    return redirect('/todo/')
