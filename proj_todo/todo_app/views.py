5f7caf19e6b6eb82e3df59cdd8faf93f6274a693
Gaurav-714 <gauravnagose714@gmail.com>
2024-01-25 13:39:55 +0530

    final

--- proj_todo/todo_app/views.py
+++ proj_todo/todo_app/views.py
@@ -9,23 +9,22 @@ def signup_page(request):
 
         try:
             username = request.POST.get('username')
-            email = request.POST.get('email')
             password = request.POST.get('password')
             
             user = User.objects.filter(username = username)
             if user.exists():
-                messages.warning(request, "Username Is Already Taken.")
+                messages.warning(request, "*** Username Is Already Taken ***")
                 return redirect('/')
             
-            user = User.objects.create(username = username, email = email)
+            user = User.objects.create(username = username)
             user.set_password(password)
             user.save()
 
-            messages.success(request, "Account Created.")
+            messages.success(request, "*** Account Created ***")
             return redirect('/signin/')
 
         except:
-            messages.warning(request, "Something Went Wrong...")
+            messages.warning(request, "*** Something Went Wrong ***")
             return redirect('/')
         
     return render(request, 'signup.html')
@@ -41,7 +40,7 @@ def signin(request):
 
             user = User.objects.filter(username = username)
             if not user.exists():
-                messages.warning(request, "Username Not Found.")
+                messages.warning(request, "*** Username Not Found ***")
                 return redirect('/signin/')
             
             user = authenticate(username = username, password = password)
@@ -49,11 +48,11 @@ def signin(request):
                 login(request, user)
                 return redirect('/todo/')
             else:
-                messages.warning(request, "Incorrect Password")
+                messages.warning(request, "*** Incorrect Password ***")
                 return redirect('/signin/')
         
         except:
-            messages.warning(request, "Something Went Wrong...")
+            messages.warning(request, "*** Something Went Wrong ***")
             return redirect('/signin/')
 
     return render(request, 'signin.html')
